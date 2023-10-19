cb82580c3b09a9bc2a87aa3a48af5328f464a726
Тимур <@DESKTOP-BQMAK7L>
2023-10-19 19:54:24 +0300

    Добавьте файлы проекта.

---
+++ 5.1/5.1.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<cmath>
+#include<map>
+
+using namespace std;
+int evkDel(int a, int b) {
+	while (a != 0 && b != 0) {
+		if (a > b) {
+			a = a % b;
+		}
+		else {
+			b = b % a;
+		}
+	}
+	return a + b;
+}
+float evk2(float a, float b) {
+	while (a != b) {
+		if (a > b) {
+			a = a - b;
+		}
+		else {
+			b = b - a;
+		}
+	}
+	return a;
+}
+
+int main() {
+	float a, b;
+	cin >> a >> b;
+	cout << evkDel(a, b) << " " << evk2(a, b);
+
+}
